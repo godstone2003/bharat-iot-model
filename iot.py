ec70e48625c4b6a3b427b94572723ba397459a0d
Anand Mishra <ayush19november@gmail.com>
2025-04-12 00:05:28 +0530

    Update iot.py

--- iot.py
+++ iot.py
@@ -4,11 +4,35 @@ import random
 import time
 from datetime import datetime
 
+# creating the table
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.create_table(
+    TableName='iot-input-table',
+    KeySchema=[
+        {'AttributeName': 'device_id', 'KeyType': 'HASH'},  # Partition key
+        {'AttributeName': 'timestamp', 'KeyType': 'RANGE'}  # Sort key
+    ],
+    AttributeDefinitions=[
+        {'AttributeName': 'device_id', 'AttributeType': 'S'},
+        {'AttributeName': 'timestamp', 'AttributeType': 'S'}
+    ],
+    BillingMode='PAY_PER_REQUEST'
+)
+
+print("Creating table...")
+table.meta.client.get_waiter('table_exists').wait(TableName='iot-input-table')
+print("✅ Table created!")
+print("Staring the iot device to send the data to model")
+
+#iot device data sending code 
+
 # AWS Lambda client
 lambda_client = boto3.client('lambda', region_name='us-east-1')  # e.g., 'us-east-1'
 
 # Lambda function name (not the ARN, just the name or alias)
-lambda_function_name = 'bharat-lamda-funtion'
+lambda_function_name = 'bharat-iot-funtion'
 
 def simulate_iot_input():
     now = datetime.now()
